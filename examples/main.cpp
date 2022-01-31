4866b54a0010caeeafc3b1ce2ccd0486825ed83c
Jeeyong Um <conr2d@gmail.com>
2022-01-31 18:14:10 +0800

    docs: update example

--- examples/main.cpp
+++ examples/main.cpp
@@ -10,18 +10,25 @@ class chain_plugin : public appbase::plugin<chain_plugin> {
 public:
    APPBASE_PLUGIN_REQUIRES();
 
-   void set_program_options(CLI::App& cli, CLI::App& config) override {
-      auto chain_options = config.add_subcommand("chain", "Chain configuration");
-      chain_options->configurable();
+   void set_program_options(CLI::App& config) override {
+      // Create subsection `chain` in TOML config file by add_section.
+      auto chain_options = config.add_section("chain", "Chain configuration");
 
-      chain_options->add_option("readonly", "open the database in read only mode");
-      chain_options->add_option("dbsize", "Minimum size MB of database shared memory file")->default_val(8 * 1024ULL);
+      // Can be called with CLI or set by config file.
+      chain_options->add_option("--readonly", "open the database in read only mode");
 
-      cli.add_flag("--replay", "clear chain database and replay all blocks");
-      cli.add_flag("--reset", "clear chain database and block log");
+      // Call group("") not to be listed in CLI options. (Config file only options)
+      chain_options->add_option("--dbsize", "Minimum size MB of database shared memory file")->default_val(8 * 1024ULL)->group("");
+
+      // A command-line option that doesn't have leading dashes `-` is considered
+      // as a positional argument. Refer to CLI11 documentation.
+
+      // Call configurable(false) not to be listed in config file. (CLI only options)
+      chain_options->add_flag("--replay", "clear chain database and replay all blocks")->configurable(false);
+      chain_options->add_flag("--reset", "clear chain database and block log")->configurable(false);
    }
 
-   void plugin_initialize(const CLI::App& cli, const CLI::App& config) { std::cout << "initialize chain plugin\n"; }
+   void plugin_initialize(const CLI::App& config) { std::cout << "initialize chain plugin\n"; }
    void plugin_startup()  { std::cout << "starting chain plugin \n"; }
    void plugin_shutdown() { std::cout << "shutdown chain plugin \n"; }
 
@@ -35,16 +42,15 @@ class net_plugin : public appbase::plugin<net_plugin> {
 public:
    APPBASE_PLUGIN_REQUIRES( (chain_plugin) );
 
-   void set_program_options(CLI::App& cli, CLI::App& config) override {
-      auto net_options = config.add_subcommand("net", "Net configuration");
-      net_options->configurable();
+   void set_program_options(CLI::App& config) override {
+      auto net_options = config.add_section("net", "Net configuration");
 
-      net_options->add_option("listen-endpoint", "The local IP address and port to listen for incoming connections.")->default_str("127.0.0.1:9876");
-      net_options->add_option("remote-endpoint", "The IP address and port of a remote peer to sync with.")->take_all();
-      net_options->add_option("public-endpoint", "The public IP address and port that should be advertized to peers.")->default_str("0.0.0.0:9876");
+      net_options->add_option("--listen-endpoint", "The local IP address and port to listen for incoming connections.")->default_str("127.0.0.1:9876");
+      net_options->add_option("--remote-endpoint", "The IP address and port of a remote peer to sync with.")->take_all();
+      net_options->add_option("--public-endpoint", "The public IP address and port that should be advertized to peers.")->default_str("0.0.0.0:9876");
    }
 
-   void plugin_initialize(const CLI::App& cli, const CLI::App& config) { std::cout << "initialize net plugin\n"; }
+   void plugin_initialize(const CLI::App& config) { std::cout << "initialize net plugin\n"; }
    void plugin_startup()  { std::cout << "starting net plugin \n"; }
    void plugin_shutdown() { std::cout << "shutdown net plugin \n"; }
 };
@@ -59,7 +65,8 @@ int main(int argc, char** argv) {
       }
       appbase::app().set_config_file("app.toml");
       appbase::app().register_plugin<net_plugin>();
-      if (!appbase::app().initialize(argc, argv))
+      appbase::app().parse_config(argc, argv);
+      if (!appbase::app().initialize())
          return -1;
       appbase::app().startup();
       appbase::app().exec();
